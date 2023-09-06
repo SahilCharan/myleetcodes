a6aee068ada429077681759fb8b9868d59343475
SahilCharan <123011559+SahilCharan@users.noreply.github.com>
2023-09-07 00:04:04 +0530

    Time: 0 ms (100.00%) | Memory: 39.4 MB (99.78%) - LeetSync

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.java
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -1,47 +1,12 @@
-class Solution {
+    class Solution{
     public double average(int[] salary) {
-        int minimumindex = mi(salary);
-        int maxindex = maxind(salary);
-        int sum = 0,count=0;
-        for(int i = 0 ; i< salary.length;i++)
-        {
-            if(i == minimumindex || i == maxindex)
-            continue;
-            else
-            {sum+=salary[i];
-            count++;}
+        double sum = 0d;
+        int M = Integer.MIN_VALUE, m = Integer.MAX_VALUE;
+        for (int s : salary) {
+            m = Math.min(m, s);
+            M = Math.max(M, s);
+            sum += s;
         }
-        return (double)sum/count;
+        return (double)(sum - m - M) / (salary.length - 2);
     }
-    public int mi(int[] salary)
-    {
-        int minimum = salary[0];
-        int mind = 0;
-        for(int i = 1;i <salary.length;i++)
-        {
-            if(salary[i]<minimum)
-            {
-                minimum = salary[i];
-                mind = i;
-            }
-        }
-        return mind;
     }
-    public int maxind(int[] salary)
-    {
-        int max = salary[0];
-        int mind = 0;
-        for(int i = 1;i<salary.length;i++)
-        {
-            if(salary[i]>max)
-            {
-                max = salary[i];
-                mind = i;
-            }
-        }
-        return mind;
-        
-
-
-    }
-}
