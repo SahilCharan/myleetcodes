47843a578b0c04acd553287842c93790e9b4462a
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-01-28 14:28:04 +0530

    Time: 4 ms (41.20%) | Memory: 45.8 MB (47.27%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,29 +1,25 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
-        int n2 = nums2.length;
-        int n = n1 + n2;
-        int[] new_arr = new int[n];
-
-        int i=0, j=0, k=0;
-
-        while (i<=n1 && j<=n2) {
-            if (i == n1) {
-                while(j<n2) new_arr[k++] = nums2[j++];
-                break;
-            } else if (j == n2) {
-                while (i<n1) new_arr[k++] = nums1[i++];
-                break;
-            }
-
-            if (nums1[i] < nums2[j]) {
-                new_arr[k++] = nums1[i++];
-            } else {
-                new_arr[k++] = nums2[j++];
-            }
+        int n2 = nums2.length;int k = 0;
+        int[] nums3 = new int[n1+n2];
+        for(int i = 0;i<n1;i++)
+        {
+            nums3[i] = nums1[i];
+            k = i;
         }
-
-        if (n%2==0) return (float)(new_arr[n/2-1] + new_arr[n/2])/2;
-        else return new_arr[n/2];
+        for(int i = 0;i<n2;i++)
+        {
+            nums3[i+n1] = nums2[i];
+        }
+        Arrays.sort(nums3);
+        if((n1+n2)%2!=0)
+        {
+            return nums3[(n1+n2)/2];
+        }
+        else
+        {
+            return (double)((nums3[(n1+n2)/2])+(nums3[(n1+n2)/2-1]))/2.0;
+        }
     }
 }
