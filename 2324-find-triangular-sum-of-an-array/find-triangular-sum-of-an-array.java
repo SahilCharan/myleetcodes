5253909c2e1c4e92214ba53e7c802fbba4461cb6
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-01-29 15:40:22 +0530

    Time: 40 ms (97.81%) | Memory: 44.7 MB (26.10%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
@@ -1,15 +1,16 @@
-
-        class Solution {
-    public int triangularSum(int[] nums) {
-        if(nums.length==1) return nums[0];
-        int n=nums.length;
-        while(n!=0){
-            for(int i=0;i<n-1;i++){
-                int m = (nums[i]+nums[i+1]);
-                nums[i]=m%10;
-            }
-            n-=1;
+class Solution {
+    public int function(int[] arr, int n){
+        if(n == 1)
+            return arr[0];
+            
+        for(int i=0;i<n-1;i++){
+            arr[i] = (arr[i] + arr[i+1])%10; 
         }
-        return nums[0];
+        return function(arr,n-1);
     }
+    public int triangularSum(int[] nums) {
+        return function(nums,nums.length);
+    }
+    
+    
 }
