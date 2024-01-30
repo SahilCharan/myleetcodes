6a08cbbc8a1f31ac9d21115b6245a60a4148ab38
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-01-30 22:33:33 +0530

    Time: 5 ms (97.05%) | Memory: 44.7 MB (27.84%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,31 +1,25 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<String> st = new Stack<>();
-        for(String token : tokens)
-        {
-           if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
-            
-            int b = Integer.parseInt(st.pop());
-                int a = Integer.parseInt(st.pop());
-                st.push(Integer.toString(perform(token, a, b)));
-            }
-            else
-            {
-                st.push(token);
+        Stack<Integer> stack=new Stack<>();
+        //String tokensStr= String.join("", tokens); //Doesn't work because I'm not only getting single-digit numbers
+        for(String c:tokens){
+            switch (c){
+                case "+":
+                    stack.push(stack.pop()+stack.pop());//.charValue()
+                    break;
+                case "-":
+                    stack.push(-stack.pop()+stack.pop()); //ordering l-r
+                    break;
+                case "*":
+                    stack.push(stack.pop()*stack.pop());
+                    break;
+                case "/":
+                    stack.push((int)((1/(double)stack.pop())*stack.pop())); //ordering l-r //plus, remeber that we're dealing with ints, so 1/anythingOtherThanOne is a ZERO
+                    break;
+                default:
+                    stack.push(Integer.parseInt(c));
             }
         }
-        return Integer.parseInt(st.pop());
-        
-    }
-    public int perform(String op, int a, int b){
-    
-        if (op.equals("+"))
-            return a + b;
-        else if (op.equals("-"))
-            return a - b;
-        else if (op.equals("*"))
-            return a * b;
-        else
-            return a / b;
+        return stack.pop();
     }
 }
