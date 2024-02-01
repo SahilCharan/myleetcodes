ece65856e611355a24a51b8ad96feabe88f326eb
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-02-02 00:37:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (43.34%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,36 +1,25 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-      int m = matrix.length;
-      int n = matrix[0].length;
-      for(int i =0;i<m;i++)
-      {
-          if(matrix[i][0]== target || matrix[i][n-1]== target)
-          return true;
-          else
-          if(matrix[i][0]<target && matrix[i][n-1]>target)
-          {int r = bsearch(matrix,i,target);
-          if(r==1)
-          return true;
-          }
-      }  
-      return false;
-    }
-    public int bsearch(int[][] matrix, int i,int target)
-    {
-        int low =0 ; int high = matrix[0].length-1;
-        while(low<=high)
-        {
-            
-           int mid = low + (high - low) / 2;
-           int midElement = matrix[i][mid];
-           if(midElement == target)
-           return 1;
-           else
-           if(midElement<target)
-           low = mid+1;
-           else
-           high = mid-1;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
         }
-        return 0;
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int row = 0;
+        int col = n - 1;
+        
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true; // Target found
+            } else if (matrix[row][col] < target) {
+                row++; // Move downwards
+            } else {
+                col--; // Move leftwards
+            }
+        }
+        
+        return false; // Target not found
     }
 }
