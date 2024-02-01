60d13a059846cc2ed537e15dc8bda6c16dc4afab
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-02-02 00:31:27 +0530

    Time: 5 ms (98.14%) | Memory: 46.1 MB (36.77%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,34 +1,13 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int i=0;
+        int j=matrix[0].length-1;
 
-        // Binary search in each row
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] <= target && matrix[i][n - 1] >= target) {
-                // If target is within the range of this row, perform binary search
-                if (binarySearch(matrix[i], target)) {
-                    return true;
-                }
-            }
+        while(i<=matrix.length-1 && j>=0){
+            if(target==matrix[i][j])return true;
+            if(target<matrix[i][j])j--;
+            else i++;
         }
         return false;
-    }
-
-    // Binary search in a sorted array
-    private boolean binarySearch(int[] arr, int target) {
-        int low = 0;
-        int high = arr.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] == target) {
-                return true; // Element found
-            } else if (arr[mid] < target) {
-                low = mid + 1; // Search in the right half
-            } else {
-                high = mid - 1; // Search in the left half
-            }
-        }
-        return false; // Element not found
     }
 }
