71efc7f1fdd26f031846eebd197c9df8f21169b2
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-02-05 22:13:02 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (62.60%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       Set<Integer> set = new HashSet<>();
+       /*Set<Integer> set = new HashSet<>();
        int ans[] = new int[2];
        for(int i = 0; i < nums.length; i++) {
            int complement = target - nums[i];
@@ -19,5 +19,20 @@ class Solution {
             if(nums[i] == x) return i;
         }
         return -1;
+        */
+
+        int left=0,right=nums.length-1;
+        for(int i=0;i<nums.length/2;i++){
+            if(nums[left]+nums[right]==target)
+                return new int[]{left,right};
+            for(int j=i+1;j<right;j++){
+                if(nums[left]+nums[j]==target)
+                    return new int[]{left,j};
+                if(nums[j]+nums[right]==target)
+                    return new int[]{j,right};
+            }
+            left++;right--;
+        }
+        return new int[]{0,0};
     }
 }
