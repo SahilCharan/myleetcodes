56cbdbfadcb0f4bf63ba2854d68e1a0e6cb1cfd4
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-02-09 23:40:13 +0530

    Time: 13 ms (99.83%) | Memory: 43.3 MB (28.83%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.java
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -1,33 +1,34 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        Arrays.sort(nums); // Sort to ensure divisors are encountered before multiples
-        int n = nums.length;
-        int[] dp = new int[n]; // Store the length of the largest divisible subset ending at each element
-        int[] prev = new int[n]; // Store the previous element in the largest divisible subset
-        int maxIdx = 0; // Index of the element ending the largest subset
-
-        for (int i = 0; i < n; i++) {
-            dp[i] = 1; // Base case: every element is a subset of size 1 by itself
-            prev[i] = -1; // Initialize previous element as -1
-            for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                    prev[i] = j;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,-1);
+        Arrays.sort(nums);
+        int[] hash = new int[nums.length];
+        int maxi = -1;
+        int lastIndex = 0;
+        for(int index=0;index<nums.length;index++){
+            hash[index] = index;
+            for(int col=0;col<index;col++){
+                
+                if(nums[index]%nums[col]==0 && 1 + dp[col] > dp[index]){
+                    dp[index] = 1 + dp[col];
+                    hash[index] = col;
                 }
             }
-            if (dp[i] > dp[maxIdx]) {
-                maxIdx = i;
+            if(dp[index]>maxi){
+                maxi = dp[index];
+                lastIndex = index;
             }
-        }
+        } 
+        List<Integer> lst = new ArrayList<Integer>();
+        lst.add(nums[lastIndex]);
+        while(hash[lastIndex]!=lastIndex){
+            lastIndex = hash[lastIndex];
+            lst.add(nums[lastIndex]);
+            
 
-        // Backtrack to construct the subset
-        List<Integer> result = new ArrayList<>();
-        int curr = maxIdx;
-        while (curr != -1) {
-            result.add(nums[curr]);
-            curr = prev[curr];
         }
-        Collections.reverse(result); // Reverse to get the correct order
-        return result;
+        return lst;
     }
+    
 }
