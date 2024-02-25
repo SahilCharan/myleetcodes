6d0e93b32821d909efa213069f170dacae03b05f
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-02-25 23:56:48 +0530

    Time: 33 ms (53.32%) | Memory: 51.4 MB (69.84%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,48 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // Sort the array
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate elements for i
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {// skip dublicates for i
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-
             int j = i + 1;
             int k = nums.length - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum == 0) {
-                    // Found a triplet with zero sum
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    // Skip duplicate elements for j
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-
-                    // Skip duplicate elements for k
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-
-                    // Move the pointers
+            while(j<k)
+            {
+            if (nums[i] + nums[j] + nums[k] == 0) {
+                ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                // skip j dublicates
+                while (j < k && nums[j] == nums[j + 1]) {
                     j++;
-                    k--;
-                } else if (sum < 0) {
-                    // Sum is less than zero, increment j to increase the sum
-                    j++;
-                } else {
-                    // Sum is greater than zero, decrement k to decrease the sum
+                }
+                // skip k dublicates
+                while (j < k && nums[k] == nums[k - 1]) {
                     k--;
                 }
-            }
+                j++;
+                k--;
+            } else if (nums[i] + nums[j] + nums[k] < 0) {
+                j++;
+            } else
+                k--;
+
+        }
         }
         return ans;
     }
+
 }
