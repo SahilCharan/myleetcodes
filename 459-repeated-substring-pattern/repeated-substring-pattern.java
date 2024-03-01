53b719a116139a922b0ee3607183e911784dd29f
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-03-01 13:13:27 +0530

    Time: 6 ms (96.06%) | Memory: 44.4 MB (97.13%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -1,7 +1,24 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-         String doubled = s + s;
-        String sub = doubled.substring(1, doubled.length() - 1);
-        return sub.contains(s);
+        int n = s.length();
+        for (int len = 1; len <= n / 2; len++) {
+            if (n % len == 0) {
+                String pattern = s.substring(0, len);
+                if (isRepeatedPattern(pattern, s)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean isRepeatedPattern(String pattern, String s) {
+        int patternLen = pattern.length();
+        for (int i = patternLen; i < s.length(); i += patternLen) {
+            if (!s.substring(i, i + patternLen).equals(pattern)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
