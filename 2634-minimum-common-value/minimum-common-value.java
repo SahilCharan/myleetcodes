fcca222fc8fb450c4cfcc22618f0f06ac038dd4d
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-03-09 16:23:05 +0530

    Time: 1 ms (96.67%) | Memory: 62.4 MB (69.04%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -3,7 +3,7 @@ class Solution {
         if (nums1[nums1.length-1] < nums2[0] || nums2[nums2.length-1] < nums1[0]) return -1;
 
         for (int i = 0; i < nums1.length; i++) {
-            System.out.println(nums1[i]);
+            
             if (Arrays.binarySearch(nums2, nums1[i]) >= 0) {
                 return nums1[i];
             }
