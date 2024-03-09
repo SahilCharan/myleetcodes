6c08d6e3805d9f4b7bb588aef69315387a38da68
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-03-10 04:11:11 +0530

    Time: 5 ms (59.14%) | Memory: 44.9 MB (31.88%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,38 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       /*Set<Integer> set = new HashSet<>();
-       int ans[] = new int[2];
-       for(int i = 0; i < nums.length; i++) {
-           int complement = target - nums[i];
-           if(set.contains(complement)) {
-               ans[0] = getIndex(nums, complement);
-               ans[1] = i;
-               break;
-           }
-           set.add(nums[i]);
-       }
-       return ans;
-    }
-    
-    public int getIndex(int[] nums, int x) { 
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] == x) return i;
-        }
-        return -1;
-        */
+        int ans[] = new int [2];
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i =0;i<nums.length;i++)
+        {
+            if(map.containsKey(target - nums[i]))
+            {
+                ans[0] = i;
+                ans[1] = map.get(target - nums[i]);
 
-        int left=0,right=nums.length-1;
-        for(int i=0;i<nums.length/2;i++){
-            if(nums[left]+nums[right]==target)
-                return new int[]{left,right};
-            for(int j=i+1;j<right;j++){
-                if(nums[left]+nums[j]==target)
-                    return new int[]{left,j};
-                if(nums[j]+nums[right]==target)
-                    return new int[]{j,right};
             }
-            left++;right--;
+            map.put(nums[i],i);
         }
-        return new int[]{0,0};
+        return ans;
     }
 }
