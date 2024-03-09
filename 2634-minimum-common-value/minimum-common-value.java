19bceebd3116bb2bb04fb953aa435c332b80f90c
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-03-09 16:22:49 +0530

    Time: 22 ms (6.67%) | Memory: 61.7 MB (99.63%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,35 +1,16 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-
-       
-        
         if (nums1[nums1.length-1] < nums2[0] || nums2[nums2.length-1] < nums1[0]) return -1;
 
-         int n = nums1.length;
-        for (int i = 0; i < n; i++) {
-            if (search(nums2, nums1[i])) {
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.println(nums1[i]);
+            if (Arrays.binarySearch(nums2, nums1[i]) >= 0) {
                 return nums1[i];
             }
         }
         return -1;
+        
     }
-    public boolean search(int[] nums, int target) {
-        int n = nums.length;
-        int l = 0;
-        int h = n - 1;
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-            if (nums[mid] == target) {
-                return true;
-            } else if (nums[mid] > target) {
-                h = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return false;
-    }
-
-    
-       
 }
+
+        
