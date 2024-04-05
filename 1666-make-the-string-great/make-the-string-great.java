a7561ddc9ba00f98a4aa858da3489952144635a1
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-04-06 02:26:03 +0530

    Time: 2 ms (88.29%) | Memory: 42 MB (76.91%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -1,27 +1,18 @@
 class Solution {
     public String makeGood(String s) {
-        StringBuilder ans = new StringBuilder();
-        
-        // Handling the case when the string is empty or has only one character
-        if (s.length() <= 1) {
-            return s;
+        StringBuilder sb =  new StringBuilder();
+        if(s.length()<=1)
+        return s;
+
+        sb.append(s.charAt(0));
+        for(int i=1;i<s.length();i++)
+        {
+            char ch = s.charAt(i);
+            if(sb.length()>0 && (sb.charAt(sb.length()-1)-32==ch ||sb.charAt(sb.length()-1)+32==ch ))
+            sb.deleteCharAt(sb.length() - 1);
+            else
+            sb.append(s.charAt(i));
         }
-        
-        // Append the first character to the result
-        ans.append(s.charAt(0));
-        
-        // Iterate through the string to eliminate adjacent characters
-        for (int i = 1; i < s.length(); i++) {
-            // Check if the current character and the last character in the result are reactive
-            if (ans.length() > 0 && Math.abs(ans.charAt(ans.length() - 1) - s.charAt(i)) == 32) {
-                // Eliminate the last character in the result
-                ans.deleteCharAt(ans.length() - 1);
-            } else {
-                // Append the current character to the result
-                ans.append(s.charAt(i));
-            }
-        }
-        
-        return ans.toString();
+        return sb.toString();
     }
 }
