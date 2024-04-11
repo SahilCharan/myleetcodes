8af8b9fc05493d8a31c04240537f7b7d6dc2866b
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-04-11 15:58:44 +0530

    Time: 19 ms (68.37%) | Memory: 45.4 MB (59.34%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -1,37 +1,37 @@
-import java.util.Stack;
-
 class Solution {
-    public String removeKdigits(String num, int k) {
+    public static String removeKdigits(String num, int k) {
+        
         int len = num.length();
-        if (k == len) return "0"; // If we need to remove all digits, return "0"
+        if (k == len){
+            return "0";
+        } 
         
-        Stack<Character> stk = new Stack<>();
-        
-        for (char ch : num.toCharArray()) {
-            while (!stk.isEmpty() && k > 0 && ch < stk.peek()) {
-                stk.pop();
+        Stack<Character> stack = new Stack<>();
+        int i = 0;
+        while (i < num.length()) {
+            while (k > 0 && !stack.isEmpty() && stack.peek() > num.charAt(i)) {
+                stack.pop();
                 k--;
             }
-            stk.push(ch);
+            stack.push(num.charAt(i));
+            i++;
         }
         
-        // If we still have remaining digits to remove, remove from the end of the stack
         while (k > 0) {
-            stk.pop();
+            stack.pop();
             k--;
         }
         
-        // Constructing the result string directly from the stack
-        StringBuilder ans = new StringBuilder();
-        while (!stk.isEmpty()) {
-            ans.insert(0, stk.pop());
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        sb.reverse();
+        
+        while (sb.length() > 1 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
         }
         
-        // Remove leading zeros
-        while (ans.length() > 1 && ans.charAt(0) == '0') {
-            ans.deleteCharAt(0);
-        }
-        
-        return ans.toString();
+        return sb.toString();
     }
 }
