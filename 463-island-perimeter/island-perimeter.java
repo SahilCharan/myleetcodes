693186e8d7145d06acf785e4fd0f49fb45cbfbef
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-04-19 01:57:17 +0530

    Time: 5 ms (51.42%) | Memory: 46.1 MB (8.45%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,32 +1,26 @@
-public class Solution {
+class Solution {
     public int islandPerimeter(int[][] grid) {
-        if (grid == null) return 0;
-        for (int i = 0 ; i < grid.length ; i++){
-            for (int j = 0 ; j < grid[0].length ; j++){
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
-                    return getPerimeter(grid,i,j);
+                    int count = 0;
+                    if (i - 1 >= 0 && grid[i - 1][j] == 1) {
+                        count++;
+                    }
+                    if (j - 1 >= 0 && grid[i][j - 1] == 1) {
+                        count++;
+                    }
+                    if (i + 1 < grid.length && grid[i + 1][j] == 1) {
+                        count++;
+                    }
+                    if (j + 1 < grid[0].length && grid[i][j + 1] == 1) {
+                        count++;
+                    }
+                    ans += 4 - count;
                 }
             }
         }
-        return 0;
-    }
-    
-    public int getPerimeter(int[][] grid, int i, int j){
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {return 1;}
-        if (grid[i][j] == 0) {
-            return 1;
-        }
-        if (grid[i][j] == -1) return 0;
-        
-        int count = 0;
-        grid[i][j] = -1;
-        
-        count += getPerimeter(grid, i-1, j);
-        count += getPerimeter(grid, i, j-1);
-        count += getPerimeter(grid, i, j+1);
-        count += getPerimeter(grid, i+1, j);
-        
-        return count;
-        
+        return ans;
     }
 }
