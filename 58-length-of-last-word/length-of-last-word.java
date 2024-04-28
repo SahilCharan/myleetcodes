3e4e5f15ed1457240c4e9fb6c138374ff8480b62
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-04-29 01:34:11 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (40.73%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,17 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String str = s.trim();
-        int k=0;
-        for(int i = str.length()-1;i>=0;i--)
+        String S = s.trim(); // trim removes the leading and trailing spaces except that of between the words.
+        // hindi mein bole to trim jo hai aage or piche dono k space ko hta deta hai , swaye jo bich mein hoga usko chor k
+
+        int k =0;
+        for(int i= S.length()-1;i>=0;i--)
         {
-            char ch = str.charAt(i);
-            if(ch==' ')
-            {
-               break;
-            }
+            if(S.charAt(i)==' ')
+            break;
+            else
             k++;
         }
         return k;
-
+        
     }
 }
