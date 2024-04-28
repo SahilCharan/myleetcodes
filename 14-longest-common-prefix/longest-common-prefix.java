468e0bd70c6d5d6dd93736c71c1257b27575f492
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-04-29 04:16:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (73.04%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,45 +1,12 @@
 class Solution {
-    public String comp(String A, String B)
-    {
-        if(A.equals(B))
-        return A;
-        
-        int len;
-        if(A.length()== B.length())
-        len = A.length();
-        else
-        if(A.length()> B.length())
-        len = B.length();
-        else
-        len = A.length();
-
-        StringBuilder sb =  new StringBuilder();
-
-        for(int i =0;i<len;i++)
-        {
-            if(A.charAt(i)==B.charAt(i))
-            sb.append(A.charAt(i));
-            else
-            break;
-        }
-        return sb.toString();
-     }
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length ==1)
-        return strs[0];
-       
-       String str = comp(strs[0],strs[1]);
-       if(str == "")
-       return "";
-       else
-       {
-        for(int i =2;i<strs.length;i++)
-        {
-            str = comp(str,strs[i]);
-            if(str =="")
-            return "";
+        String prefix = strs[0];
+        for(int i = 0; i < strs.length; i++){
+            while(strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() -1);
+                if(prefix.isEmpty()) return "";
+            }
         }
-       }
-       return str;
+        return prefix;
     }
 }
