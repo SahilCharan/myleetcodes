b6a99cbe8739cee85cdca92485be4e0da1b3ddfa
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-04-29 01:28:41 +0530

    Time: 3 ms (97.16%) | Memory: 44 MB (82.51%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,58 +1,48 @@
 class Solution {
     public String intToRoman(int num) {
-        StringBuilder sb = new StringBuilder();
-        while (num >= 1000) {
-            sb.append("M");
-            num -= 1000;
+        StringBuilder result = new StringBuilder();
+        while (num > 0) {
+            if (num >= 1000) {
+                result.append('M');
+                num -= 1000;
+            } else if (num >= 900) {
+                result.append("CM");
+                num -= 900;
+            } else if (num >= 500) {
+                result.append('D');
+                num -= 500;
+            } else if (num >= 400) {
+                result.append("CD");
+                num -= 400;
+            } else if (num >= 100) {
+                result.append('C');
+                num -= 100;
+            } else if (num >= 90) {
+                result.append("XC");
+                num -= 90;
+            } else if (num >= 50) {
+                result.append('L');
+                num -= 50;
+            } else if (num >= 40) {
+                result.append("XL");
+                num -= 40;
+            } else if (num >= 10) {
+                result.append('X');
+                num -= 10;
+            } else if (num == 9) {
+                result.append("IX");
+                num -= 9;
+            } else if (num >= 5) {
+                result.append('V');
+                num -= 5;
+            } else if (num == 4) {
+                result.append("IV");
+                num -= 4;
+            } else {
+                result.append('I');
+                num -= 1;
+            }
         }
-        if (num >= 900) {
-            sb.append("CM");
-            num -= 900;
-        }
-        if (num >= 500) {
-            sb.append("D");
-            num -= 500;
-        }
-        if (num >= 400) {
-            sb.append("CD");
-            num -= 400;
-        }
-        while (num >= 100) {
-            sb.append("C");
-            num -= 100;
-        }
-        if (num >= 90) {
-            sb.append("XC");
-            num -= 90;
-        }
-        if (num >= 50) {
-            sb.append("L");
-            num -= 50;
-        }
-        if(num>=40){sb.append("XL");num -= 40; }
-        while (num >= 10) {
-            sb.append("X");
-            num -= 10;
-
-        }
-        if (num >= 9) {
-            sb.append("IX");
-            num -= 9;
-
-        }
-        if (num >= 5) {
-            sb.append("V");
-            num -= 5;
-        }
-        if (num >= 4) {
-            sb.append("IV");
-            num -= 4;
-
-        }
-        while (num >= 1) {
-            sb.append("I");
-            num -= 1;
-        }
-        return sb.toString();
+        return result.toString();
     }
 }
