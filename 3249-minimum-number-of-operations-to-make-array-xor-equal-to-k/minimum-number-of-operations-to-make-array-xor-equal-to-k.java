ae8d02bf40f36dd32fed662d2fa1b2de4061cd45
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-04-30 00:59:49 +0530

    Time: 1 ms (100.00%) | Memory: 55.2 MB (80.83%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -1,28 +1,20 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
-        int finalXor =0;
-        //XOR of all the integers in the array.
-        for(int n:nums)
+        int xor = 0;
+        for(int n: nums)
         {
-            finalXor = finalXor^n;
+            xor = xor^n;
         }
         int count =0;
-        // keep iterating untill both k and finalXor becomes zero
-        while(k>0 || finalXor>0)
+        while(k>0 || xor>0)
         {
-            //k%2 returns the rightmost bit of k
-            //finalXor %2 return the rightmost bit in finalXor
-            // increment the counter , if they are uequal
+            if((k%2)!=(xor%2))
+            count++;
 
-            if((k%2)!= (finalXor %2))
-            {
-                count++;
-            }
-            // remove the last bit from both the integers
             k/=2;
-            finalXor/=2;
-        }
+            xor/=2;
 
+        }
         return count;
     }
 }
