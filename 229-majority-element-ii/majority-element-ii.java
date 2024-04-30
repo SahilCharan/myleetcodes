5ff61cced43b5c19000885821ba145a1be9aa61b
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-04-30 17:31:18 +0530

    Time: 2 ms (99.45%) | Memory: 47.7 MB (57.30%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,14 +1,53 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer,Integer> count = new HashMap<>();
-        List<Integer> ans = new ArrayList<>();
-        for(int num:nums)
-        {
-            count.put(num,count.getOrDefault(num,0)+1);
-            if(count.get(num)>nums.length/3 && !ans.contains(num))
-            ans.add(num);
-            
+        int count1=0, count2 =0;
+        int cand1 =0, cand2 =0;
+         // finding potential majoriry element
+         for(int i =0;i<nums.length;i++)
+         {
+            if((count1 == 0) && (nums[i]!= cand2))
+            {
+                count1 =1;
+                cand1 = nums[i];
+            }
+            else if(count2 ==0 && nums[i]!=cand1)
+            {
+                count2=1;
+                cand2 = nums[i];
+            }
+            else if(cand1==nums[i])
+            count1++;
+            else if(cand2 == nums[i])
+            count2++;
+
+            else
+            {count1--;
+            count2--;}
         }
+
+        List<Integer> ans = new ArrayList<>();
+        int th = nums.length/3;
+
+        //second pass to count occurance of potential  majority element
+
+        count1 = 0;
+        count2 = 0;
+
+        for(int i =0;i<nums.length;i++)
+        {
+            if(cand1==nums[i])
+            count1++;
+            else if(cand2==nums[i])
+            count2++;
+        }
+
+        //check if the count of the otential majority elements
+
+        if(count1> th)
+        ans.add(cand1);
+
+        if(count2>th)
+        ans.add(cand2);
        
         return ans;
         
