d956bb20dfcc4b71d431bfcfba13c5dde4e16331
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-05-03 01:14:13 +0530

    Time: 2 ms (96.14%) | Memory: 45 MB (30.86%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.java
@@ -6,28 +6,41 @@ class Solution {
 
         // while(i<j)
         // {
-        //     if(nums[i]==-nums[j])
-        //     return Math.abs(nums[i]);
-        //     else
-        //     if(Math.abs(nums[i])>Math.abs(nums[j]))
-        //     i++;
-        //     else
-        //     j--;
+        // if(nums[i]==-nums[j])
+        // return Math.abs(nums[i]);
+        // else
+        // if(Math.abs(nums[i])>Math.abs(nums[j]))
+        // i++;
+        // else
+        // j--;
         // }
         // return -1;
 
-       Set<Integer> set = new HashSet<>();
-       int max = -1;
-       for(int num : nums)
-       {
-        int curr = Math.abs(num);
-        if(set.contains(-num) && curr>max)
-             max = curr;
+        // Set<Integer> set = new HashSet<>();
+        // int max = -1;
+        // for(int num : nums)
+        // {
+        // int curr = Math.abs(num);
+        // if(set.contains(-num) && curr>max)
+        // max = curr;
 
-        set.add(num);
-       }
-       return max;
+        // set.add(num);
+        // }
+        // return max;
 
+        int array[] = new int[1001];
+        int max = -1;
+        for (int num : nums) {
+            if (num < 0) {
+                array[-num] = num;
+            }
+        }
+        for (int num : nums) {
+            if (num > 0 && array[num] != 0) {
+                max = Math.max(max, num);
+            }
+        }
+        return max;
 
     }
 }
