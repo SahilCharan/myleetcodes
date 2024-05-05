f40153813640d2729cd1ff61dd4a41613788dc8a
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-05-05 16:05:33 +0530

    Time: 31 ms (66.77%) | Memory: 51.3 MB (67.82%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,35 +1,42 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {// skip dublicates for i
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        List<List<Integer>> ans =  new ArrayList<>();
+        int i =0;
+        for(i=0;i<nums.length;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1])
+            {
                 continue;
             }
-            int j = i + 1;
-            int k = nums.length - 1;
-            while(j<k)
-            {
-            if (nums[i] + nums[j] + nums[k] == 0) {
-                ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                // skip j dublicates
-                while (j < k && nums[j] == nums[j + 1]) {
+            
+            
+                int j =i+1;
+                int k = nums.length-1;
+                while(j<k)
+                {
+                if(nums[i]+nums[j]+nums[k]==0){
+                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    while(j<k&& nums[j]==nums[j+1]){
+                        j++;
+                    }
+                    while(j<k&& nums[k]==nums[k-1])
+                    {
+                        k--;
+                    }
                     j++;
-                }
-                // skip k dublicates
-                while (j < k && nums[k] == nums[k - 1]) {
                     k--;
                 }
-                j++;
-                k--;
-            } else if (nums[i] + nums[j] + nums[k] < 0) {
-                j++;
-            } else
+                else if(nums[i]+nums[j]+nums[k]<0)
+                {
+                    j++;
+                }
+                else
                 k--;
 
+            
         }
         }
         return ans;
     }
-
 }
