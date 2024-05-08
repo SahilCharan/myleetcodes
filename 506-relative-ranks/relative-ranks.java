97cf520a46f305d56db7e5af573abf40c2348436
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-05-08 17:38:06 +0530

    Time: 8 ms (80.99%) | Memory: 46 MB (8.75%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i =0;i<score.length;i++)
+        {
+            map.put(score[i],i);
+        }
+        Arrays.sort(score);
+        String ans[] = new String[score.length];
+        int n = score.length;
+        for(int i =0;i<score.length;i++)
+        {
+           if(i==n-1)
+           {
+            ans[map.get(score[i])] = "Gold Medal";
+           }
+           else if(n-2>=0&& i==n-2)
+           {
+            ans[map.get(score[i])] = "Silver Medal";
+           }
+           else if(n-3>=0 && i== n-3)
+           {
+            ans[map.get(score[i])] = "Bronze Medal";
+           }
+           else{
+            ans[map.get(score[i])] = Integer.toString(n-i);
+           }
+        }
+        return ans;
+    }
+}
