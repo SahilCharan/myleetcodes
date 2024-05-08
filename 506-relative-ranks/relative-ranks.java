a96c61e898666303b3d11583225ef16bb5e7e23f
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-05-08 17:50:38 +0530

    Time: 3 ms (98.37%) | Memory: 45.6 MB (58.39%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -1,28 +1,40 @@
 class Solution {
+    private int findMax(int[] score) {
+        int maxScore = 0;
+        for (int s : score) {
+            if (s > maxScore) {
+                maxScore = s;
+            }
+        }
+        return maxScore;
+    }
+
     public String[] findRelativeRanks(int[] score) {
-        int n = score.length;
-        PriorityQueue<Pair<Integer,Integer>> heap = new PriorityQueue<>((a,b)->b.getKey()-a.getKey());
-        for(int i= 0;i<n;i++)
-        {
-            heap.add(new Pair<>(score[i],i));
+        int N = score.length;
+
+        // Add the original index of each score to the array
+        // Where the score is the key
+        int M = findMax(score);
+        int[] scoreToIndex = new int[M + 1];
+        for (int i = 0; i < N; i++) {
+            scoreToIndex[score[i]] = i + 1;
         }
 
-        String rank[] = new String[n];
-        int place =1;
-        while(!heap.isEmpty())
-        {
-            Pair<Integer,Integer> pair = heap.poll();
-            int ori = pair.getValue();
-            if(place ==1)
-            rank[ori] = "Gold Medal";
-            else if(place ==2)
-            rank[ori] ="Silver Medal";
-            else if(place ==3)
-            rank[ori] = "Bronze Medal";
-            else
-            rank[ori] = String.valueOf(place);
+        final String[] MEDALS = {"Gold Medal", "Silver Medal", "Bronze Medal"};
 
-            place++;
+        // Assign ranks to athletes
+        String[] rank = new String[N];
+        int place = 1;
+        for (int i = M; i >= 0; i--) {
+            if (scoreToIndex[i] != 0) {
+                int originalIndex = scoreToIndex[i] - 1;
+                if (place < 4) {
+                    rank[originalIndex] = MEDALS[place - 1];
+                } else {
+                    rank[originalIndex] = String.valueOf(place);
+                }
+                place++;
+            }
         }
         return rank;
     }
