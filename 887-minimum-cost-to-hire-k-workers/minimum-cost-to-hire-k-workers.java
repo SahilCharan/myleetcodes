056eaec4d7830103c5f7add129fcb17736905e62
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-05-11 21:59:43 +0530

    Time: 28 ms (66.80%) | Memory: 45.6 MB (73.52%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.java
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.java
@@ -1,30 +1,22 @@
 class Solution {
-    public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-        List<Pair<Double, Integer>> ratio = new ArrayList<>();
-        int n = quality.length, qualitySum = 0;
-        double res = Double.MAX_VALUE, maxRate = 0.0;
-
-        for (int i = 0; i < n; ++i) {
-            ratio.add(new Pair<>((double) wage[i] / quality[i], i));
+    public double mincostToHireWorkers(int[] quality, int[] wage, int K) {
+        // current ratio * total quality = total wage for this group.
+        int n = wage.length;
+        double[][] workers = new double[n][2];
+        for (int i = 0; i < n; i++){
+            workers[i] = new double[]{(double)wage[i]/quality[i], (double)quality[i]};
         }
-
-        ratio.sort(Comparator.comparingDouble(p -> p.getKey()));
-        for (int i = 0; i < k; ++i) {
-            qualitySum += quality[ratio.get(i).getValue()];
-            maxRate = Math.max(maxRate, ratio.get(i).getKey());
-            maxHeap.offer(quality[ratio.get(i).getValue()]);
+        // sort by wage rate, 
+        Arrays.sort(workers, (a, b) -> Double.compare(a[0], b[0]));
+        // maxheap, poll() max quality; get min qsum;
+        PriorityQueue<Double> pq = new PriorityQueue<>((a,b)->Double.compare(b, a));
+        double qsum = 0, res = Double.MAX_VALUE;
+        for (double[] work : workers){
+            qsum += work[1];
+            pq.offer(work[1]);
+            if (pq.size() > K) qsum -= pq.poll();
+            if (pq.size() == K) res = Math.min(res, qsum * work[0]);
         }
-
-        res = maxRate * qualitySum;
-        for (int i = k; i <n; ++i) {
-            maxRate = Math.max(maxRate, ratio.get(i).getKey());
-            qualitySum -= maxHeap.poll();
-            qualitySum += quality[ratio.get(i).getValue()];
-            maxHeap.offer(quality[ratio.get(i).getValue()]);
-            res = Math.min(res, maxRate * qualitySum);
-        }
-
         return res;
     }
 }
