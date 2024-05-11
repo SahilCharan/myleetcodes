9219cab957a691114bd9e4f6e87c6b102aed9058
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-05-11 14:17:38 +0530

    Time: 714 ms (6.55%) | Memory: 68.6 MB (54.15%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -1,40 +1,18 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        int n = arr.length;
-        double left = 0, right = 1, mid;
-        int[] res = new int[2];
-
-        while (left <= right) {
-            mid = left + (right - left) / 2;
-            int j = 1, total = 0, num = 0, den = 0;
-            double maxFrac = 0;
-            for (int i = 0; i < n; ++i) {
-                while (j < n && arr[i] >= arr[j] * mid) {
-                    ++j;
-                }
-                
-                total += n - j;
-
-                if (j < n && maxFrac < arr[i] * 1.0 / arr[j]) {
-                    maxFrac = arr[i] * 1.0 / arr[j];
-                    num = i;
-                    den = j;
-                }
-            }
-
-            if (total == k) {
-                res[0] = arr[num];
-                res[1] = arr[den];
-                break;
-            }
-
-            if (total > k) {
-                right = mid;
-            } else {
-                left = mid;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingDouble(a -> (double) a[0] / a[1]));
+        for(int i =0;i<arr.length;i++)
+        {
+            for(int j =i+1;j<arr.length;j++)
+            {
+                minHeap.offer(new int[]{arr[i],arr[j]});
             }
         }
-
-        return res;
+        int result[] = null;
+        for(int i =0;i<k;++i)
+        {
+            result=minHeap.poll();
+        }
+        return result;
     }
 }
