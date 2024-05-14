a86ee68a2185ec59b5bb51f494a4529218c2a074
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-05-14 15:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (81.06%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,5 +1,5 @@
 class Solution {
-    // public int uniquePaths(int m, int n) {
+    public int uniquePaths(int m, int n) {
         // recuersion
 
     //     return findpath(0,0,m,n);
@@ -16,24 +16,38 @@ class Solution {
 
 
     //dp
-    public int countPaths(int i, int j, int n, int m, int[][] dp) {
-        if (i == (n - 1) && j == (m - 1)) return 1;
-        if (i >= n || j >= m) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-        else return dp[i][j] = countPaths(i + 1, j, n, m, dp) + countPaths(i, j + 1, n, m, dp);
-    }
+    // public int countPaths(int i, int j, int n, int m, int[][] dp) {
+    //     if (i == (n - 1) && j == (m - 1)) return 1;
+    //     if (i >= n || j >= m) return 0;
+    //     if (dp[i][j] != -1) return dp[i][j];
+    //     else return dp[i][j] = countPaths(i + 1, j, n, m, dp) + countPaths(i, j + 1, n, m, dp);
+    // }
 
-    public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        int num = countPaths(0, 0, m, n, dp);
-        if (m == 1 && n == 1)
-            return num;
-        return dp[0][0];
+    // public int uniquePaths(int m, int n) {
+    //     int[][] dp = new int[m][n];
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             dp[i][j] = -1;
+    //         }
+    //     }
+    //     int num = countPaths(0, 0, m, n, dp);
+    //     if (m == 1 && n == 1)
+    //         return num;
+    //     return dp[0][0];
+    // }
+
+    //combination
+
+    int nu = m+n-2;
+    int r= m-1;
+
+    double res =1;
+    for(int i=1;i<=r;i++)
+    {
+        res = res*(nu-r+i)/i;
+    }
+    return (int)res;
+
     }
     
 }
