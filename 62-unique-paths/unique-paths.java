509fe58a9b90a8f1756df68e7c855264832398c4
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-05-14 15:38:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (68.04%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,17 +1,39 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
+    // public int uniquePaths(int m, int n) {
+        // recuersion
+
+    //     return findpath(0,0,m,n);
         
-        long result = 1;
+    // }
+    // public int findpath(int i, int j, int m, int n)
+    // {
+    //     if(i==(m-1)|| j == (n-1)) return 1;
+    //     if(i>m&&j>n) return 0;
 
-        
-        int smaller = Math.min(m - 1, n - 1);
+    //     return findpath(i+1,j,m,n)+findpath(i,j+1,m,n);
 
-       
-        for (int i = 1; i <= smaller; i++) {
-            result = result * (m + n - 2 - i + 1) / i;
-        }
+    // }
 
-       
-        return (int) result;
+
+    //dp
+    public int countPaths(int i, int j, int n, int m, int[][] dp) {
+        if (i == (n - 1) && j == (m - 1)) return 1;
+        if (i >= n || j >= m) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        else return dp[i][j] = countPaths(i + 1, j, n, m, dp) + countPaths(i, j + 1, n, m, dp);
     }
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        int num = countPaths(0, 0, m, n, dp);
+        if (m == 1 && n == 1)
+            return num;
+        return dp[0][0];
+    }
+    
 }
