89887a13cb796776ebfd11ae660deb44d15e0484
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-05-15 20:56:42 +0530

    Time: 4 ms (74.61%) | Memory: 43.1 MB (73.49%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,32 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!= t.length())
+        return false;
 
-     class Solution {
-    // Function to check whether two strings are anagrams of each other or not.
-    public static boolean isAnagram(String a, String b) {
-        // Your code here
-        if (a.length() != b.length()) {
+        int freq[] = new int[26];
+        for(char c:s.toCharArray())
+        {
+            freq[c-'a']++;
+        }
+        for(char c:t.toCharArray())
+        {
+            freq[c-'a']--;
+        }
+        for(int f : freq)
+        {
+            if(f!=0)
             return false;
         }
-
-        // Assuming ASCII characters, you can use an array of size 256 for the frequency count.
-        int[] frequency = new int[256];
-
-        // Increment frequency for characters in string a
-        for (char ch : a.toCharArray()) {
-            frequency[ch]++;
-        }
-
-        // Decrement frequency for characters in string b
-        for (char ch : b.toCharArray()) {
-            frequency[ch]--;
-        }
-
-        // If the strings are anagrams, all frequencies should be zero
-        for (int count : frequency) {
-            if (count != 0) {
-                return false;
-            }
-        }
-
         return true;
+
     }
 }
