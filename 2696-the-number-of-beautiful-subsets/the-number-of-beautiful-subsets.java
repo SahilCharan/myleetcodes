a51b397e2572cde7ba8e4da0e103c9078eca6e35
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-05-24 02:42:40 +0530

    Time: 3 ms (100.00%) | Memory: 44 MB (90.14%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
@@ -1,25 +1,27 @@
 class Solution {
-    public int dfs(int[] nums, int idx, int k, HashMap<Integer, Integer> mp) {
-        if (idx == nums.length) return 1;
-
-        int taken = 0;
-        if (!mp.containsKey(nums[idx] - k) && !mp.containsKey(nums[idx] + k)) {
-            mp.put(nums[idx], mp.getOrDefault(nums[idx], 0) + 1);
-            taken = dfs(nums, idx + 1, k, mp);
-            mp.put(nums[idx], mp.get(nums[idx]) - 1);
-            if (mp.get(nums[idx]) == 0) {
-                mp.remove(nums[idx]);
-            }
-        }
-        
-        int notTaken = dfs(nums, idx + 1, k, mp);
-        
-        return taken + notTaken;
-    }
-
     public int beautifulSubsets(int[] nums, int k) {
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        int ans = dfs(nums, 0, k, mp);
-        return ans - 1;
+        
+        Map<Integer, Integer> m = new HashMap<>();
+
+        for (int num : nums) m.put(num, m.getOrDefault(num, 0) + 1);
+
+        int res = 1, prev = 0, prevPrev = 0;
+
+        for (Map.Entry<Integer, Integer> e : m.entrySet()) {
+            int cur = e.getKey();
+
+            if (m.containsKey(cur - k)) continue;
+            
+            prev = 0;
+
+            while (m.containsKey(cur)) {
+                prevPrev = prev;
+                prev = ((1 << m.get(cur)) - 1) * res;
+                res += prevPrev;
+                cur += k;
+            }
+            res += prev;
+        }
+        return res - 1;
     }
 }
