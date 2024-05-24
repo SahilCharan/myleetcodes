819bc054276d2352c48a1bf72390959fe352dd2f
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-05-24 22:04:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (73.22%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,25 +10,27 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode temp = head;
-        //gettting the tail of the linkedlist
-        while(temp!=null)
-        {
-            temp = temp.next;
-        }
-        //now i am standing on the tail of the linkedlist
-        ListNode prev = null;
-        ListNode curr = temp= head;
-        ListNode nexts;
-        while(curr!=null)
-        {
-            nexts = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nexts;
-        }
-        head = prev;
 
-        return head;
+        // I can solve it using stack. 
+        // what i will be doing is, adding all the element in the stack
+        // then i am going creat a new list node 
+        // and connecting it to the list by popping the element from the stack, and the loop will run tiull the stack is non empty
+
+        // otheer way is:
+
+        ListNode prev = null;
+        ListNode temp = head;
+        //1->2->3->4
+        while(temp!= null)
+        {
+            ListNode current  =  temp.next; //curr = 1,2,3,4
+            temp.next = prev; // 3,2,1->null
+            prev = temp; // prev 1,2,3,4;
+            
+            temp = current; //temp =4
+
+        }
+        return prev;
+        
     }
 }
