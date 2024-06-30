f27bbd369493a412f0b6204f3a953f67d0dfeb6a
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-06-30 15:55:06 +0530

    Time: 1 ms (80.53%) | Memory: 45.6 MB (34.21%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,91 +1,35 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length; // row length
-        int n = matrix[0].length; // column length
+        
+    //way 2, ek col row krke array bnate h fir aage ka kaam dekho
 
-        // Method 1
-        // this code runs only when the -1 is not include in matrix
-        // for (int i = 0; i < m; i++) {
-        // for (int j = 0; j < n; j++) {
-        // if (matrix[i][j] == 0) {
-        // markRow(matrix, i, n);
-        // markCol(matrix, j, m);
-        // }
-        // }
-        // }
-        // for (int i = 0; i < m; i++) {
-        // for (int j = 0; j < n; j++) {
-        // if (matrix[i][j] == Integer.MAX_VALUE) {
-        // matrix[i][j] = 0;
-        // }
-        // }
-        // }
-        // }
+    int m=  matrix.length;
+    int n =  matrix[0].length;
+    int row[] = new int[m];
+    int col[] =  new int[n];
+    
 
-        // public void markRow(int matrix[][], int i, int n) {
-        // for (int j = 0; j < n; j++) {
-        // if(matrix[i][j]!=0)
-        // matrix[i][j] = Integer.MAX_VALUE;
-        // }
-        // }
-
-        // public void markCol(int matrix[][], int j, int m) {
-        // for (int i = 0; i < m; i++) {
-        // if(matrix[i][j]!=0)
-        // matrix[i][j] =Integer.MAX_VALUE ;
-        // }
-
-        // method 2
-        // boolean row[] = new boolean[m];
-        // boolean col[] = new boolean[n];
-
-        // for(int i = 0;i<m;i++)
-        // {
-        // for(int j =0;j<n;j++)
-        // {
-        // if(matrix[i][j]==0)
-        // {
-        // row[i]= true;
-        // col[j]= true;
-        // }
-        // }
-        // }
-        // for(int i=0;i<m;i++)
-        // {
-        // for(int j =0;j<n;j++)
-        // {
-        // if(row[i] || col[j])
-        // {
-        // matrix[i][j]=0;
-        // }
-        // }
-        // }
-
-        // }
-        // }
-        // method 3
-        int cols = 1;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    if (j != 0)
-                        matrix[0][j] = 0;
-                    else
-                        cols = 0;
-                }
+    for(int i =0;i<m;i++)
+    {
+        for(int j =0;j<n;j++)
+        {
+            if(matrix[i][j] ==0)
+            {
+                row[i] = -1;
+                col[j] =-1;
             }
+        }
+    }
+    for(int i =0;i<m;i++)
+    {
+        for(int j =0;j<n;j++)
+        {
+            if(row[i] == -1 || col[j] ==-1)
+            matrix[i][j] =0;
+        }
+    }
 
-        for (int i = 1; i < m; i++)
-            for (int j = 1; j < n; j++)
-                if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                    matrix[i][j] = 0;
 
-        if (matrix[0][0] == 0)
-            for (int j = 0; j < n; j++)
-                matrix[0][j] = 0;
-        if (cols == 0)
-            for (int i = 0; i < m; i++)
-                matrix[i][0] = 0;
+
     }
 }
