851189f4667d73f50f704b7e87034c7e4123d6a4
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-07-05 00:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (36.93%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,35 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int firstOccurrence = findFirstOccurrence(nums, target);
-        int lastOccurrence = findLastOccurrence(nums, target);
+        int[] ans = new int[2];
+        int low = 0;
+        int high = nums.length - 1;
+        Arrays.fill(ans, -1);
 
-        return new int[]{firstOccurrence, lastOccurrence};
-    }
-
-    private int findFirstOccurrence(int[] nums, int target) {
-        int low = 0, high = nums.length - 1, first = -1;
-
-        while (low <= high) {
-            int mid = low + ((high - low)>>2);
-
+        while (high >= low) {
+            int mid = (high + low) / 2;
             if (nums[mid] == target) {
-                first = mid;
-                high = mid - 1; // Search towards the left
-            } else if (nums[mid] < target) {
-                low = mid + 1;
+                int x = mid;
+                int y = mid;
+                while (x >= 0 && nums[x] == target) {
+                    x--;
+                }
+                while (y < nums.length && nums[y] == target) {
+                    y++;
+                }
+                ans[0] = x + 1;
+                ans[1] = y - 1;
+                return ans;
             } else {
-                high = mid - 1;
+                if (nums[mid] < target) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
 
-        return first;
-    }
-
-    private int findLastOccurrence(int[] nums, int target) {
-        int low = 0, high = nums.length - 1, last = -1;
-
-        while (low <= high) {
-            int mid = low + ((high - low)>>2);
-
-            if (nums[mid] == target) {
-                last = mid;
-                low = mid + 1; // Search towards the right
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-
-        return last;
+        return ans;
     }
 }
