a83b352e552cd48ab5771935513c95d24f6612e3
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-07-06 12:30:22 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (53.37%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,9 +1,40 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        Arrays.sort(nums);
-       int n = Arrays.binarySearch(nums, target);
-        if(n>=0)
+        int index  = getminvalue(nums);
+        int a  = binarySearch(nums,0,index,target);
+        if(a>=0)
         return true;
-        else return false;
+        int b =  binarySearch(nums,index,nums.length-1,target);
+        if(b>=0)
+        return true;
+        else
+        return false;
     }
+    public int getminvalue(int[] nums)
+    {
+        int ind =-1;
+        for(int i =0;i<nums.length-1;i++)
+        {
+            if(nums[i]>nums[i+1])
+            {ind  =i;
+            break;}
+        }
+        return ind+1;
+    }
+    public int binarySearch(int[] arr, int start, int end, int target)
+    {
+        while(start<= end)
+        {
+            int mid = start+(end-start)/2;
+            if(arr[mid]== target)
+            return mid;
+            else
+            if(arr[mid]>target)
+            end =  mid-1;
+            else
+            start = mid+1;
+        }
+        return -1;
+    }
+   
 }
