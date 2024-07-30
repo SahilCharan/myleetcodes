fcae9fe9df347988bcb57da19aa9a789ce5b7a8b
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-07-30 14:07:45 +0530

    Time: 2 ms (79.57%) | Memory: 45.6 MB (49.12%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxReach =0;
+        int maxreach =0;
         for(int i =0;i<nums.length;i++)
         {
-            if(i>maxReach) //yadi i badad hogya maxreach k value se, to 0 return krdena mtlb hm i ka value tk pohoch he nhi pa rahe hai
+            if(i> maxreach)
             return false;
-            
-            maxReach = Math.max(maxReach, i+nums[i]);//updating maxcReach
-            if(maxReach >= nums.length-1)
+
+            maxreach = Math.max(maxreach, i+nums[i]);
+            if(maxreach >= nums.length-1)
             return true;
         }
         return false;
