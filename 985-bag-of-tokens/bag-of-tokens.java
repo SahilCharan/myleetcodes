af967a057e35254f1daa262905c67923eebe9c5a
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-07-30 23:21:54 +0530

    Time: 4 ms (11.17%) | Memory: 42.7 MB (49.07%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.java
+++ 985-bag-of-tokens/bag-of-tokens.java
@@ -1,29 +1,24 @@
 class Solution {
-     public int bagOfTokensScore(int[] tokens, int power) {
-        int maxScore = 0;
-        int score =0;
-        int n = tokens.length;
-        Arrays.sort(tokens);
-        int i = 0;
-        int j = n -1;
-        while(i <= j){
-            if(power >= tokens[i]){
-                power = power - tokens[i];
-                score++;
-                i++;
-                maxScore = Math.max(maxScore,score);
-            }
-            else if(score >=1){
-                power = power+tokens[j];
-                score--;
-                j--;
-                maxScore = Math.max(maxScore,score);
-            }
-            else{
-                return maxScore;
-            }
+    public int bagOfTokensScore(int[] tokens, int power) {
+      int i =0, score =0;
+      Arrays.sort(tokens);
+      int maxscore = 0;
+      int j =tokens.length-1;
+      while(i<=j)  {
+        if(tokens[i]<=power)
+        {
+            score++;
+            power-=tokens[i];
+            i++;
+            maxscore =  Math.max(maxscore,score);
         }
-        return maxScore;
-
+        else if(score>=1)
+        {
+            power+=tokens[j];
+            j--;
+            score--;
+        }else  return maxscore;
+      }
+      return maxscore;
     }
 }
