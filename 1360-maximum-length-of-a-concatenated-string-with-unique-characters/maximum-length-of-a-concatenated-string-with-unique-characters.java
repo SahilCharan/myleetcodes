a4e2a0af0cc161819cd6b53fe516bfcc022f96af
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-08-02 03:21:51 +0530

    Time: 28 ms (26.57%) | Memory: 51.8 MB (5.34%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,45 +1,54 @@
+import java.util.*;
+
 class Solution {
     public int maxLength(List<String> arr) {
-        String temp= "";
+        String temp = "";
         int n = arr.size();
+        HashMap<String, Integer> map = new HashMap<>();
 
-        return solve(arr, temp, 0, n);
+        return solve(arr, temp, 0, n, map);
     }
-    int solve(List<String> arr, String temp, int index, int n)
-    {
-        if(index>=n)
-        return temp.length();
 
-        int include =0;
-        int exclude =0;
-
-        if(hasDublicate(arr.get(index),temp))
-        exclude = solve(arr, temp,index+1, n);
-        else
-        {
-            include =  solve(arr, temp+arr.get(index),index+1,n);
-            exclude = solve(arr, temp,index+1, n);
+    int solve(List<String> arr, String temp, int index, int n, HashMap<String, Integer> map) {
+        if (index >= n) {
+            return temp.length();
         }
-        return Math.max(include, exclude);
+
+        String key = temp + "|" + index;
+        if (map.containsKey(key)) {
+            return map.get(key);
+        }
+
+        int include = 0;
+        int exclude = 0;
+
+        if (hasDuplicate(arr.get(index), temp)) {
+            exclude = solve(arr, temp, index + 1, n, map);
+        } else {
+            include = solve(arr, temp + arr.get(index), index + 1, n, map);
+            exclude = solve(arr, temp, index + 1, n, map);
+        }
+
+        int result = Math.max(include, exclude);
+        map.put(key, result);
+        return result;
     }
-    boolean hasDublicate(String s1, String s2)
-    {
-        int arr[] = new int[26];
 
-        for(int i =0;i<s1.length();i++)
-        {
-            char ch = s1.charAt(i);
-            if(arr[ch-'a']>0)
-            return true;
+    boolean hasDuplicate(String s1, String s2) {
+        int[] charCount = new int[26];
 
-            arr[ch-'a']++;
+        for (char ch : s1.toCharArray()) {
+            if (charCount[ch - 'a']++ > 0) {
+                return true;
+            }
         }
-       for(int i =0;i<s2.length();i++)
-        {
-            char ch = s2.charAt(i);
-            if(arr[ch-'a']>0)
-            return true;
+
+        for (char ch : s2.toCharArray()) {
+            if (charCount[ch - 'a']++ > 0) {
+                return true;
+            }
         }
+
         return false;
     }
 }
