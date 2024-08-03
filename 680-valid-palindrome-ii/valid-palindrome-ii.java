7e7f57f29c972cf2afe2b15fa440bd22775bef93
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-08-03 18:33:05 +0530

    Time: 4 ms (99.56%) | Memory: 45.4 MB (25.13%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,28 +1,19 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int i =0;
-        int j = s.length()-1;
-        while(i<j)
-        {
-            if(s.charAt(i)!=s.charAt(j))
-            {
-                return isPalind(s,i+1,j)||isPalind(s,i,j-1);
-            }
-            i++;
-            j--;
-        
-        }
-        return true;
+        return funCall(s,0,s.length()-1,0);
     }
-    boolean isPalind(String s, int i, int j)
+    boolean funCall(String s,int i, int j, int check)
     {
+        if(check>1)
+        return false;
+
         while(i<j)
         {
-            if(s.charAt(i)!=s.charAt(j))
-            return false;
-
-            i++;
-            j--;
+            if(s.charAt(i)!= s.charAt(j))
+            {
+                return funCall(s,i+1,j,check+1)||funCall(s,i,j-1,check+1);
+            }
+            i++;j--;
         }
         return true;
     }
