d2f5e0b0c3ca022cd7eaaebc6614e727e5c1f93e
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-11-17 22:05:43 +0530

    Time: 1 ms (91.11%) | Memory: 44.4 MB (97.13%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -15,41 +15,41 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans =  new ArrayList<>();
-        if(root == null)
-        return ans;
-
         Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>>list = new ArrayList<>();
+        //list khali hai to return kr do khali
+        if(root == null)
+        return list;
+
+        //q mein root ko add kr do, or level ko partiion krke k liye null add kr dete h
+
         q.add(root);
         q.add(null);
-        List<Integer> list = new ArrayList<>();
-        
-        while(!q.isEmpty())
-        {
-            TreeNode curr =  q.poll();
-            if(curr == null)
-            {
-                ans.add(list);
-                list =  new ArrayList<>();
-            
-            if(!q.isEmpty())
-            {
-                q.add(null);
-            }
-            }
-            else
-            {
-                list.add(curr.val);
-                if(curr.left!=null)
+        List<Integer>l = new ArrayList<>();
+        while(!q.isEmpty()){
+            TreeNode curr = q.poll();
+            if(curr==null){
+                //curr yadi null hai to dekho ab hoga jadu
+                // pehle list ko add kr do answer list mein or fir l mein naya level dalne k liye naya list ho uske liye usko khali kr do
+                list.add(l);
+                l= new ArrayList<>();
+
+                if(!q.isEmpty())
                 {
+                    q.add(null);
+                }
+            }
+            else{
+                l.add(curr.val);
+                if(curr.left!=null){
                     q.add(curr.left);
                 }
-                if(curr.right!=null)
-                {
+                if(curr.right!=null){
                     q.add(curr.right);
                 }
             }
         }
-        return ans;
+        return list;
+
     }
 }
