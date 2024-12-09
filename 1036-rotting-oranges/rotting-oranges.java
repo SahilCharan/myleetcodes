b58cb1e9b66bb60f7441e38725647e3040791605
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-12-09 19:08:19 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (35.73%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,39 +1,38 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        //there will be multiple entry entry points, that is multiple rotten oranges
-        Queue<int[]>q = new LinkedList<>();
+        Queue<int[]> q = new LinkedList<>();
         int m = grid.length;
-        int n = grid[0].length;
-        int vis[][] = grid;
+        int n= grid[0].length;
+        int copy[][] = grid;
+        int time =-1;
         int count =0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(vis[i][j]==2){
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(copy[i][j]==2){
                     q.offer(new int[] {i,j});
                 }
-                if(vis[i][j]==1){
-                    count++;
-                }
+                if(copy[i][j]==1)
+                count++;
             }
         }
-        if(count==0)
+        if(count ==0)
         return 0;
         if(q.isEmpty())
         return -1;
 
-        int time =-1;
-        int directions[][] = {{1,0},{-1,0},{0,-1},{0,1}};
-        while((!q.isEmpty())){
+        int dir[][] = {{1,0},{-1,0},{0,-1},{0,1}};
+        while(!q.isEmpty())
+        {
             int size = q.size();
             while(size-->0){
-                int cell[] = q.poll();
-                int x = cell[0];
-                int y = cell[1];
-                for(int[] dir:directions){
-                    int i = x+dir[0];
-                    int j = y+dir[1];
-                    if(i>=0 &&i<m &&j>=0 &&j<n && vis[i][j]==1){
-                        vis[i][j]=2;
+                int curr[] = q.poll();
+                int x = curr[0];
+                int y = curr[1];
+                for(int d[]:dir){
+                    int i = x+d[0];
+                    int j = y+d[1];
+                    if(i>=0 &&i<m && j>=0 && j<n && copy[i][j]==1){
+                        copy[i][j] = 2;
                         count--;
                         q.offer(new int[] {i,j});
                     }
@@ -45,8 +44,6 @@ class Solution {
         return time;
 
         return -1;
-
-
 
     }
 }
