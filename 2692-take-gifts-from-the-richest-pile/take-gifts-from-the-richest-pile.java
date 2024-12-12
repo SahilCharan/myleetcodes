0f5bcb124fa5a52bed1a73d8011124a21270bb99
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-12-12 06:42:47 +0530

    Time: 5 ms (89.83%) | Memory: 42.8 MB (16.45%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
@@ -1,22 +1,10 @@
 class Solution {
     public long pickGifts(int[] gifts, int k) {
-      PriorityQueue<Integer>pq = new PriorityQueue<>(Collections.reverseOrder());
-      for(int i =0;i<gifts.length;i++){
-        pq.offer(gifts[i]);
-      }
-      while(k>0)
-      {
-        int x = pq.poll();
-        int sq = (int)Math.floor(Math.sqrt(x));
-        pq.offer(sq);
-        k--;
-      }
-      long sum =0;
-      while(!pq.isEmpty()){
-        sum+=pq.poll();
-      }
-      return sum;
-
-
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        for(int gift: gifts)    pq.add(gift);
+        while(k-->0)    pq.add((int)Math.floor(Math.sqrt(pq.poll())));
+        long sum = 0;
+        while(!pq.isEmpty())    sum += pq.poll();
+        return sum;
     }
 }
