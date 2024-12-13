17e3247c0063cc3d34b56fbbfc97e2905615e98c
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-12-13 21:14:52 +0530

    Time: 9 ms (58.93%) | Memory: 63.1 MB (5.16%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -2,25 +2,70 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if ((i == 0 || j == 0 || i == m - 1 || j == n - 1) && grid[i][j] == 1) {
-                    dfs(grid, i, j);
-                }
+
+        for(int i =0;i<m;i++){
+            if(grid[i][0]==1) dfs(grid,i,0);
+            if(grid[i][n-1]==1) dfs(grid,i,n-1);
+        }
+        for(int j=0;j<n;j++){
+            if(grid[0][j]==1) dfs(grid,0,j);
+            if(grid[m-1][j]==1) dfs(grid,m-1,j);
+        }
+        int sum =0;
+        for(int i =0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1)
+                sum++;
             }
         }
-        return Arrays.stream(grid).mapToInt(row -> Arrays.stream(row).sum()).sum();
+        return sum;
     }
-    
-    private void dfs(int[][] grid, int i, int j) {
-        grid[i][j] = 0;
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        for (int[] direction : directions) {
-            int x = i + direction[0];
-            int y = j + direction[1];
-            if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == 1) {
-                dfs(grid, x, y);
+    void dfs(int grid[][], int i, int j){
+        int dir[][] = {{-1,0},{1,0},{0,-1},{0,1}};
+        grid[i][j]=-1;
+        for(int d[]:dir){
+            int x = i+d[0];
+            int y = j+d[1];
+            if(x>=0 &&x<grid.length && y>=0 && y<grid[0].length && grid[x][y]==1){
+                dfs(grid,x,y);
+
             }
         }
     }
 }
+
+/*
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        int r = grid.length;
+        int c = grid[0].length;
+        for(int i=0;i<r;i++){
+            if(grid[i][0]==1) dfs(grid,i,0);
+            if(grid[i][c-1]==1) dfs(grid,i,c-1);
+        }
+        for(int j=0;j<c;j++){
+            if(grid[0][j]==1) dfs(grid,0,j);
+            if(grid[r-1][j]==1) dfs(grid,r-1,j);
+        }
+        int count = 0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    private void dfs(int[][] grid, int r, int c){
+        if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c]!=1){
+            return;
+        }
+        grid[r][c] = -1;
+        dfs(grid,r-1,c);
+        dfs(grid,r+1,c);
+        dfs(grid,r,c-1);
+        dfs(grid,r,c+1);
+    }
+}
+*/
