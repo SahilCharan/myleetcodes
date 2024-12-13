32e548a48023f8d6ddb531c5734c8b0f69d25fbd
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-12-13 11:50:00 +0530

    Time: 3 ms (99.51%) | Memory: 59.3 MB (58.33%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -1,33 +1,15 @@
 class Solution {
-
     public long findScore(int[] nums) {
-        long ans = 0;
-        int[][] sorted = new int[nums.length][2];
-        boolean[] marked = new boolean[nums.length];
-
-        for (int i = 0; i < nums.length; i++) {
-            sorted[i][0] = nums[i];
-            sorted[i][1] = i;
-        }
-
-        Arrays.sort(sorted, (arr1, arr2) -> arr1[0] - arr2[0]);
-
-        for (int i = 0; i < nums.length; i++) {
-            int number = sorted[i][0];
-            int index = sorted[i][1];
-            if (!marked[index]) {
-                ans += number;
-                marked[index] = true;
-                // mark adjacent elements if they exist
-                if (index - 1 >= 0) {
-                    marked[index - 1] = true;
-                }
-                if (index + 1 < nums.length) {
-                    marked[index + 1] = true;
-                }
+        long res = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            int start = i;
+            while (i + 1 < nums.length && nums[i + 1] < nums[i]) {
+                i++;
+            }
+            for (int j = i; j >= start; j -= 2) {
+                res += nums[j];
             }
         }
-
-        return ans;
+        return res;
     }
 }
