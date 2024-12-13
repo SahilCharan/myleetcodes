922cd559581ce22c9f7c69e5307229cd0013170e
Kumar Sahil <123011559+SahilCharan@users.noreply.github.com>
2024-12-13 11:49:00 +0530

    Time: 108 ms (84.80%) | Memory: 59.6 MB (52.78%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -1,42 +1,33 @@
 class Solution {
+
     public long findScore(int[] nums) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> { // arranging the numbers as in ascending order but if
-                                                                  // two numbers are same then one with lower index,
-                                                                  // gets higher priority
-            if (a[0] != b[0]) {
-                return Integer.compare(a[0], b[0]);
-            } else {
-                return Integer.compare(a[1], b[1]);
-            }
-        });
-        long score = 0;
-        boolean vis[] = new boolean[nums.length];//taking the visted array to mark the elements which are not marked
-        int totalMarked = 0;
+        long ans = 0;
+        int[][] sorted = new int[nums.length][2];
+        boolean[] marked = new boolean[nums.length];
+
         for (int i = 0; i < nums.length; i++) {
-            pq.offer(new int[] { nums[i], i });
+            sorted[i][0] = nums[i];
+            sorted[i][1] = i;
         }
-        while (!pq.isEmpty()) {
-            int curr[] = pq.poll();
-            int a = curr[0];// choosing the smallest element
-            int b = curr[1];
-            if (!vis[b]) {
-                score += a;// if not marked then add the element;
-                vis[b] = true;// mark as visited
-                totalMarked++;
-                if (b + 1 < nums.length) { // mark the neighbours
-                    vis[b + 1] = true;
-                   // score += nums[b + 1];
-                    totalMarked++;
+
+        Arrays.sort(sorted, (arr1, arr2) -> arr1[0] - arr2[0]);
+
+        for (int i = 0; i < nums.length; i++) {
+            int number = sorted[i][0];
+            int index = sorted[i][1];
+            if (!marked[index]) {
+                ans += number;
+                marked[index] = true;
+                // mark adjacent elements if they exist
+                if (index - 1 >= 0) {
+                    marked[index - 1] = true;
                 }
-                if (b - 1 >= 0) {
-                    vis[b - 1] = true;
-                   // score += nums[b - 1];
-                    totalMarked++;
+                if (index + 1 < nums.length) {
+                    marked[index + 1] = true;
                 }
             }
-            //if (totalMarked == nums.length)
-              //  break;
         }
-        return score;
+
+        return ans;
     }
 }
